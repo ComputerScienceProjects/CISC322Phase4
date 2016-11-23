aafa5ff0574bf5d4eae6841fd729522b2dc03ae0
TaylorJoshua <8jst@queensu.ca>
2016-11-23 17:30:57 -0500

    Added error message.

--- src/editor/MoveColumnRightAction.java
+++ src/editor/MoveColumnRightAction.java
@@ -4,6 +4,7 @@ import java.awt.event.ActionEvent;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.swing.JOptionPane;
 import javax.swing.JTextArea;
 // For documentation purposes, import only those classes from edfmwk that are
 // actually used.
@@ -19,41 +20,47 @@ import ca.queensu.cs.dal.flex.log.Log;
  */
 public class MoveColumnRightAction extends CSVAction {
     /**
-     * Constructs a column movement action -- move selected column right.
+     * Constructs a column movement action -- move selected column(s) right.
      */
     public MoveColumnRightAction() {
-	super("Move Column Right");
-    } // end constructor MoveColumnLeftAction
+	super("Move Column(s) Right");
+    } // end constructor MoveColumnRightAction
 
     /**
      * Move the selected column right.
-     * If multiple columns are selected, move multiple columns left.
-     * @param con Text document to change.
-     * @param start Index of the first character to change.
-     * @param end Index one beyond the last character to change.
+     * If multiple columns are selected, move multiple columns right.
+     * @param con CSV document to change.
+     * @param startRow Index of the first row to change.
+     * @param startCol Index of the first column to change.
+     * @param endRow Index one beyond the last row to change.
+     * @param endCol Index one beyond the last column to change.
      */
     protected void changeCSV(CSVContents con, int startRow, int startCol, int endRow, int endCol) {
     try {
-    	if(endCol < con.getColumnCount()) {	// A valid column is selected. (con.getColumnCount() is invalid, since it's farthest right.)
-    		// Store the column that will be displaced left.
-    		Object[] oldCol = con.getColumnAt(endCol);
-    		int height = con.getRowCount();
-    		for(int col = endCol-1; col >= startCol; col--) {
-    			// Get the column to be moved.
-    			Object[] thisCol = con.getColumnAt(col);
-    			// Move its values to the new column.
-    			for(int row = 0; row < height; row++) {
-    				con.setValueAt(thisCol[row], row, col+1);
-    			}
+    	// Store the column that will be displaced leftwards by the move.
+    	Object[] oldCol = con.getColumnAt(endCol);
+    	int height = con.getRowCount();
+    	for(int col = endCol-1; col >= startCol; col--) {
+    		// Get the column to be moved rightwards.
+    		Object[] thisCol = con.getColumnAt(col);
+    		// Move its values to the column beside it to the right.
+    		for(int row = 0; row < height; row++) {
+    			con.setValueAt(thisCol[row], row, col+1);
     		}
-    		// Replace the leftmost selected column with the displaced column.
-			for(int row = 0; row < height; row++) {
-				con.setValueAt(oldCol[row], row, startCol);
-			}
-	    	//Tell the listeners that we changed the data in the table (by moving columns)
-	    	con.fireTableDataChanged();
     	}
+    	// Replace the leftmost selected column with the displaced column.
+    	for(int row = 0; row < height; row++) {
+    		con.setValueAt(oldCol[row], row, startCol);
+    	}
+    	//Tell the listeners that we changed the data in the table.
+    	con.fireTableDataChanged();
+    	
     } catch (Exception e) {
+    	if(startCol < 0) {
+    		JOptionPane.showMessageDialog(null, "Please select a column in order to perform that action.", "No Column Selected", JOptionPane.ERROR_MESSAGE);
+    	} else if (endCol < con.getColumnCount()) {
+    		JOptionPane.showMessageDialog(null, "Column index exceeded!", "This Should Not Happen", JOptionPane.ERROR_MESSAGE);
+    	}
     	e.printStackTrace();
     }
     } // end changeCSV
